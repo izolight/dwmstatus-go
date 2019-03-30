5242d9edd8b4208087affa1fccc755c034fae705
Gabor Tanz <izolight@gmail.com>
2019-03-30 18:24:21 +0100

    Add method for printing with delimiter

--- main.go
+++ main.go
@@ -23,6 +23,8 @@ func printIPs(ipType string, ips[]string) string {
 	return ""
 }
 
+type status string
+
 func main() {
 	ifName := "wlp4s0"
 	prevRx, prevTx, err := plugins.GetRxTxBytes(ifName)
@@ -43,40 +45,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var status string
+	var status status
 	for {
 		status = ""
 		ipv4s, ipv6s, err := plugins.GetIPs(ifName, "enp0s31f6")
 		if err != nil {
-			status += fmt.Sprintf("Couldn't get ip addresses", err)
+			log.Fatal(err)
 		} else {
-			status += printIPs("IP", ipv4s)
-			status += printIPs(" | IPv6", ipv6s)
+			status.addWithDelimiter("|", printIPs("IP", ipv4s))
+			status.addWithDelimiter("|", printIPs(" | IPv6", ipv6s))
 		}
 		ssid, err := plugins.GetSSIDFromDbus(apPath, conn)
 		if err == nil {
-			status += fmt.Sprintf(" | SSID: %s", ssid)
+			status.addWithDelimiter("|", fmt.Sprintf("SSID: %s", ssid))
 		}
 		bitrate, err := plugins.GetBitrateFromDbus(ifPath, conn)
 		if err == nil {
-			status += fmt.Sprintf(" | Speed: %s/s", humanize.Bytes(humanize.KByte *uint64(bitrate)))
+			status.addWithDelimiter("|", fmt.Sprintf("Speed: %s/s", humanize.Bytes(humanize.KByte *uint64(bitrate))))
 		}
 		rx, tx, err := plugins.GetRxTxBytes(ifName)
 		if err == nil {
-			status += fmt.Sprintf(" | Down: %s/s | Up: %s/s", humanize.Bytes(rx-prevRx), humanize.Bytes(tx-prevTx))
+			status.addWithDelimiter("|", fmt.Sprintf("Down: %s/s Up: %s/s", humanize.Bytes(rx-prevRx), humanize.Bytes(tx-prevTx)))
 		}
 		prevRx = rx
 		prevTx = tx
-/*		wifiInfo, err := plugins.GetWifiInfo("wlp4s0")
-		if err != nil {
-			status += fmt.Sprintf("Couldn't get wifi info: %s", err)
-		} else {
-			status += wifiInfo.String()
-			rxRate := humanize.Bytes(uint64(wifiInfo.RX - rx))
-			txRate := humanize.Bytes(uint64(wifiInfo.TX - tx))
-			status += fmt.Sprintf(" | Down: %s/s | Up: %s/s", rxRate, txRate)
-			rx, tx = wifiInfo.RX, wifiInfo.TX
-		}*/
 		fmt.Println(status)
 
 		sleepUntil(5)
@@ -87,4 +79,11 @@ func sleepUntil(seconds int) {
 	sleepDuration := time.Duration(seconds) * time.Second
 	now := time.Now()
 	time.Sleep(now.Truncate(sleepDuration).Add(sleepDuration).Sub(now))
+}
+
+func (s *status) addWithDelimiter(delimiter string, value string) {
+	if len(*s) != 0 {
+		*s += status(" " + delimiter + " ")
+	}
+	*s += status(value)
 }
