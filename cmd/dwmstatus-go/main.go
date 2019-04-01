3ff63ae137f1981f3abb28fd4b531940cfbed75e
Gabor Tanz <izolight@gmail.com>
2019-04-01 20:53:07 +0200

    start to use wifi library

--- cmd/dwmstatus-go/main.go
+++ cmd/dwmstatus-go/main.go
@@ -5,63 +5,48 @@ import (
 	"log"
 	"time"
 
-	//"github.com/izolight/dwmstatus-go/pkg/dbus"
-	//"github.com/izolight/dwmstatus-go/pkg/stdlib"
-
-	//"github.com/izolight/dwmstatus-go/pkg/sysfs"
-	"github.com/prometheus/procfs/sysfs"
+	"github.com/mdlayher/wifi"
 )
 
 type status string
 
 func main() {
-	interfaces := []string{"enp4s0", "enp5s0"}
-
-	var status status
-	for {
-		status = ""
-		/* ipv4s, ipv6s, err := stdlib.GetIPs(ifName, "enp0s31f6")
-		if err != nil {
-			log.Fatal(err)
-		} else {
-			if len(ipv4s) > 0 {
-				status.addWithDelimiter("|", fmt.Sprintf("IP: %s", ipv4s))
-			}
-			if len(ipv6s) > 0 {
-				status.addWithDelimiter("|", fmt.Sprintf("IPv6: %s", ipv6s))
-			}
-		}
-		apPath, err := dbus.PathForAccessPoint(ifPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		ssid, err := dbus.SSID(apPath)
-		if err == nil {
-			status.addWithDelimiter("|", fmt.Sprintf("SSID: %s", ssid))
-		}
-		bitrate, err := dbus.WifiLinkSpeed(ifPath)
-		if err == nil {
-			status.addWithDelimiter("|", fmt.Sprintf("Speed: %s/s", humanize.Bytes(humanize.KByte*uint64(bitrate))))
-		}
-		rx, tx, err := sysfs.RxTxBytes(ifName)
-		if err == nil {
-			status.addWithDelimiter("|", fmt.Sprintf("Down: %s/s Up: %s/s", humanize.Bytes(rx-prevRx), humanize.Bytes(tx-prevTx)))
-		}
-		prevRx = rx
-		prevTx = tx */
-		netClass, err := sysfs.NewNetClass()
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, i := range interfaces {
-			n := netClass[i]
-			status.addWithDelimiter("|", fmt.Sprintf("%s: %s %d", i, n.OperState, *n.Speed))
-		}
-
-		fmt.Println(status)
-
-		sleepUntil(1)
+	//interfaces := []string{"enp4s0", "enp5s0"}
+	wifiClient, err := wifi.New()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	interfaces, err := wifiClient.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(interfaces)
+
+	stationInfos, err := wifiClient.StationInfo(interfaces[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(stationInfos)
+	/*
+
+		var status status
+		for {
+			status = ""
+			netClass, err := sysfs.NewNetClass()
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, i := range interfaces {
+				n := netClass[i]
+				status.addWithDelimiter("|", fmt.Sprintf("%s: %s %d", i, n.OperState, *n.Speed))
+			}
+
+			fmt.Println(status)
+
+			sleepUntil(1)
+		} */
 }
 
 func sleepUntil(seconds int) {
--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.12
 require (
 	github.com/dustin/go-humanize v1.0.0
 	github.com/godbus/dbus v4.1.0+incompatible
+	github.com/mdlayher/genetlink v0.0.0-20190313224034-60417448a851 // indirect
+	github.com/mdlayher/wifi v0.0.0-20190303161829-b1436901ddee
 	github.com/prometheus/procfs v0.0.0-20190328153300-af7bedc223fb
 )
